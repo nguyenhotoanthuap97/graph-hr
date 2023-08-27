dfcb627736775e48fa392acfd88e50b6a952535e
Thu Nguyen <nguyenhotoanthuap97@gmail.com>
2023-08-28 01:15:00 +0700

    Update sidebar

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -3,17 +3,17 @@ import Employee from "views/Directory";
 
 var routes = [
   {
-    path: "/project",
-    name: "Project",
-    icon: "nc-icon nc-single-copy-04", //nucleo-icons
-    component: Project,
-    layout: "/admin"
-  },
-  {
     path: "/employee",
     name: "Employee",
     icon: "nc-icon nc-single-02",
     component: Employee,
+    layout: "/admin"
+  },
+  {
+    path: "/project",
+    name: "Project",
+    icon: "nc-icon nc-single-copy-04", //nucleo-icons
+    component: Project,
     layout: "/admin"
   }
 ];
